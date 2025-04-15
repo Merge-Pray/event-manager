5e2064cf196726d47ff2044a445247d846cb64ff
benNurtjipta <ben@nurtjipta.de>
2025-04-15 15:24:47 +0200

    slug needs to be fixed

--- app/concerts/page.jsx
+++ app/concerts/page.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
+  const [concertData, setConcertData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=DE&classificationName=music&apikey=003mNegIeRSw2UpynrSHta2CJEuefG48"
+        );
+        const data = await response.json();
+        setConcertData(data._embedded.events);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h2>Ich bin die Konzertübersicht</h2>
-    </div>
+    <section>
+      <ul>
+        {concertData.map((concert) => (
+          <Link href={`/concerts/${concert.id}`}>
+            <li key={concert.id}>
+              {concert.name}{" "}
+              <img className="h-20" src={concert.images[0].url}></img>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </section>
   );
 };
 
